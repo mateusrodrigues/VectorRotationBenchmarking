e19af0b0d6d974fcf160a51231060b4f3165b44e
Mateus Rodrigues de Morais <mateus.rodrigues@live.com>
2017-07-13 01:54:32 -0300

    Define the Quaternion object

---
+++ VectorRotationBenchmarking/Helpers/Quaternion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorRotationBenchmarking.Helpers
+{
+    public class Quaternion
+    {
+        public double Q0 { get; set; }
+        public double Q1 { get; set; }
+        public double Q2 { get; set; }
+        public double Q3 { get; set; }
+
+        public Quaternion(double q0, double q1, double q2, double q3)
+        {
+            Q0 = q0;
+            Q1 = q1;
+            Q2 = q2;
+            Q3 = q3;
+        }
+
+        public static Quaternion operator +(Quaternion q1, Quaternion q2)
+        {
+            return new Quaternion(q1.Q0 + q2.Q0, q1.Q1 + q2.Q1, q1.Q2 + q2.Q2, q1.Q3 + q2.Q3);
+        }
+
+        public static Quaternion operator -(Quaternion q1, Quaternion q2)
+        {
+            return new Quaternion(q1.Q0 - q2.Q0, q1.Q1 - q2.Q1, q1.Q2 - q2.Q2, q1.Q3 - q2.Q3);
+        }
+
+        public double Norm()
+        {
+            return Math.Sqrt(Math.Pow(Q0, 2) + Math.Pow(Q1, 2) + Math.Pow(Q2, 2) + Math.Pow(Q3, 2));
+        }
+
+        public override string ToString()
+        {
+            return $"({Q0}, {Q1}, {Q2}, {Q3})";
+        }
+    }
+}
--- VectorRotationBenchmarking/Program.cs
+++ VectorRotationBenchmarking/Program.cs
@@ -15,8 +15,6 @@ namespace VectorRotationBenchmarking
             double[] rotatedVector1 = RotationOperations.MatrixRotate(vector, 20);
             double[] rotatedVector2 = RotationOperations.MatrixRotate(rotatedVector1, 40);
 
-            // double[] rotatedVector = rotatedVector1 * rotatedVector2;
-
             Console.WriteLine("Matrix-based Rotation (60 degrees):");
             Console.WriteLine($"1st rotation (+ 20 degrees): ({rotatedVector1[0]}, {rotatedVector1[1]}, {rotatedVector1[2]})");
             Console.WriteLine($"2nd rotation (+ 40 degrees): ({rotatedVector2[0]}, {rotatedVector2[1]}, {rotatedVector2[2]})");
